87124a590a39dab1614e04a07a025314d3b0f16a
ConCorner <84924990+ConCorner@users.noreply.github.com>
2021-06-11 22:25:23 -0400

    Add files via upload

---
+++ solar system.py
@@ -0,0 +1,36 @@
+### This file was copied from CS 1 '19-'20 on 2021-04-06.
+
+# Fill me in!
+
+Rect(0, 0, 400, 400)
+
+
+Circle(200, 200, 50, fill=None, border='white')
+Circle(235, 235, 10, fill=gradient('Silver', 'darkGray'))
+
+#the sun
+Circle(200, 200, 35, fill=gradient('darkOrange', 'gold', 'yellow', 'gold'))
+
+Circle(200, 200, 80, fill=None, border='white')
+Circle(278, 200, 15, fill=gradient('paleGoldenRod', 'khaki', 'paleGoldenRod', start='top'))
+
+Circle(200, 200, 100, fill=None, border='white')
+Circle(200, 300, 15, fill='dodgerBlue')
+Oval(198, 300, 12, 25, fill='green')
+
+Circle(200, 200, 130, fill=None, border='white')
+Circle(200, 72, 13, fill=gradient('darkRed', 'orangeRed', 'maroon', start='bottom'))
+Circle(200, 72, 13, fill='red', opacity=30)
+
+Circle(200, 200, 160, fill=None, border='white')
+Circle(42, 200, 20, fill=gradient('tan', 'brown', 'tan', 'sienna', 'tan', start='bottom'))
+
+Circle(200, 200, 185, fill=None, border='white')
+Circle(80, 340, 18, fill=gradient('darkgray', 'tan', 'chocolate', 'peru', 'tan', start='top'))
+Oval(80, 340, 65, 5, fill='tan')
+
+Circle(200, 200, 210, fill=None, border='white')
+Circle(370, 320, 15, fill='lightBlue')
+
+Circle(200, 200, 230, fill=None, border='white')
+Circle(355, 30, 15, fill=gradient('mediumBlue', 'mediumBlue', 'cornflowerBlue', 'mediumBlue', 'darkGray', 'mediumBlue', start='bottom'))
